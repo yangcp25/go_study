e9bee26e48e0ddff84ba9ed25723450eee34d038
ycp <820363773@qq.com>
2022-07-06 23:16:53 +0800

    add sort

---
+++ algorithm/selectionSort/selectionSort.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// 选择
+// 时间复杂度 O(n2)
+// 稳定的算法
+func main() {
+	intArray := []int{4, 5, 6, 7, 8, 3, 2, 1}
+	selectionSort(intArray[:])
+	fmt.Printf("%v", intArray)
+}
+
+func selectionSort(arrayData []int) {
+	length := len(arrayData)
+	for i := 0; i < length; i++ {
+		min := i
+		j := i + 1
+		for ; j < length; j++ {
+			if arrayData[i] > arrayData[j] {
+				min = j
+			}
+		}
+		if min != j {
+			arrayData[i], arrayData[j] = arrayData[j], arrayData[i]
+		}
+	}
+}
