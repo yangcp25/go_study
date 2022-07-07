dfaa70c2aa5333751948edeff8a407bffe1c4b3d
ycp <820363773@qq.com>
2022-07-07 21:48:23 +0800

    add sort

---
+++ algorithm/mergeSortSort/go.mod
@@ -0,0 +1,3 @@
+module mergeSort
+
+go 1.18
--- algorithm/selectionSort/selectionSort.go
+++ algorithm/selectionSort/selectionSort.go
@@ -7,22 +7,55 @@ import "fmt"
 // 稳定的算法
 func main() {
 	intArray := []int{4, 5, 6, 7, 8, 3, 2, 1}
-	selectionSort(intArray[:])
+	intArray = mergeSort(intArray[:])
 	fmt.Printf("%v", intArray)
 }
 
-func selectionSort(arrayData []int) {
+func mergeSort(arrayData []int) []int {
 	length := len(arrayData)
-	for i := 0; i < length; i++ {
-		min := i
-		j := i + 1
-		for ; j < length; j++ {
-			if arrayData[i] > arrayData[j] {
-				min = j
-			}
+	if length <= 1 {
+		return arrayData
+	}
+
+	count := length / 2
+
+	left := mergeSort(arrayData[:count])
+	right := mergeSort(arrayData[count:])
+
+	return merge(left, right)
+}
+
+func merge(left []int, right []int) []int {
+	i, j := 0, 0
+	leftLength := len(left)
+	rightLength := len(right)
+	var res []int
+
+	//
+	for {
+		if i >= leftLength || j >= rightLength {
+			break
 		}
-		if min != j {
-			arrayData[i], arrayData[j] = arrayData[j], arrayData[i]
+		if left[i] <= right[j] {
+			res = append(res, left[i])
+			i++
+		} else {
+			res = append(res, right[j])
+			j++
 		}
 	}
+
+	if i < leftLength {
+		for ; i < leftLength; i++ {
+			res = append(res, left[i])
+		}
+	}
+
+	if j < rightLength {
+		for ; j < rightLength; j++ {
+			res = append(res, right[j])
+		}
+	}
+
+	return res
 }
