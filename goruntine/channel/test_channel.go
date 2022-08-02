3ed68004a9a0ba4c2f4e1c3f318af07010df0aa5
ycp19940225 <820363773@qq.com>
2022-08-02 16:00:21 +0800

    update 并发编程

--- goruntine/channel/test_channel.go
+++ goruntine/channel/test_channel.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
-	//
-	test6()
 
+	// 避免关闭已经关闭的通道
+	test8()
+	// 超时机制实现
+	//test7()
+	//
+	//test6()
 	// 协程
 	//test1()
 	// 使用channel 传递消息
@@ -25,9 +30,86 @@ func main() {
 	//
 }
 
+func test8() {
+	ch := make(chan int, 2)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <- i
+		}
+		fmt.Println("发送方关了哟！")
+		close(ch)
+	}()
+
+	for {
+		num, ok := <-ch
+		if !ok {
+			fmt.Println("你关了我也关了嘛那")
+			break
+		}
+		fmt.Println("接收到了", num)
+	}
+}
+
+func test7() {
+	ch := make(chan int)
+
+	closeChan := make(chan bool)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		closeChan <- true
+	}()
+
+	select {
+	case data, ok := <-ch:
+		if !ok {
+			break
+		}
+		fmt.Println("数据来啦！、n\n", data)
+	case <-closeChan:
+		fmt.Println("超时了！")
+	}
+}
+
 func test6() {
 	// 使用select 判断通道的值
+	chs := [3]chan int{
+		make(chan int, 3),
+		make(chan int, 3),
+		make(chan int, 3),
+	}
+	index1 := rand.Intn(3)
+	chs[index1] <- rand.Int()
 
+	index2 := rand.Intn(3)
+	chs[index2] <- rand.Int()
+
+	index3 := rand.Intn(3)
+	chs[index3] <- rand.Int()
+
+	for i := 0; i < 3; i++ {
+		select {
+		case nums, ok := <-chs[0]:
+			fmt.Println("选到第一个了before：", nums)
+			if !ok {
+				break
+			}
+			fmt.Println("选到第一个了：", nums)
+		case nums, ok := <-chs[1]:
+			if !ok {
+				break
+			}
+			fmt.Println("选到第2个了：", nums)
+		case nums, ok := <-chs[2]:
+			if !ok {
+				break
+			}
+			fmt.Println("选到第3个了：", nums)
+		default:
+			fmt.Println("个都没选到：")
+		}
+	}
 }
 
 func test5() {
