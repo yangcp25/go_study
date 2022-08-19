d7f5f5e6216dacdb92646b8a1f8b653f29700e81
ycp <820363773@qq.com>
2022-08-19 23:26:31 +0800

    update web

--- web/gorilla/gorilla.go
+++ web/gorilla/gorilla.go
@@ -9,7 +9,61 @@ import (
 
 func main() {
 	//initWeb()
-	initWeb2()
+	//initWeb2()
+	// 路由中间件
+	//initWeb3()
+	// 处理静态资源
+	initWeb4()
+}
+
+func initWeb4() {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/index", handIndex)
+	mux.Use(logMiddleware)
+
+	var dir string = "static"
+	//flag.StringVar(&dir, "dir", ".", "静态资源目录，默认为当前目录")
+	//flag.Parse()
+
+	mux.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir(dir))))
+	// 应用于子路由
+	http.ListenAndServe(":8089", mux)
+}
+
+func logMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		log.Println("log...")
+		next.ServeHTTP(writer, request)
+	})
+}
+func postLogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		token := request.FormValue("token")
+		if token == "ycp" {
+			log.Println("success token")
+			next.ServeHTTP(writer, request)
+		} else {
+			http.Error(writer, "err token", http.StatusForbidden)
+		}
+	})
+}
+func initWeb3() {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/index", handIndex)
+	mux.Use(logMiddleware)
+	// 应用于子路由
+	prePost := mux.PathPrefix("/post").Subrouter()
+	prePost.HandleFunc("", handPost)
+	prePost.Use(postLogMiddleware)
+	http.ListenAndServe(":8089", mux)
+}
+
+func handIndex(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "index")
+}
+
+func handPost(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "post")
 }
 
 func initWeb2() {
