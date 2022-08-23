a1f66bd19f04863ca97d7bc20fced8b9b3c819ed
ycp <820363773@qq.com>
2022-08-23 23:09:33 +0800

    update data

--- web/data/data.go
+++ web/data/data.go
@@ -1,9 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
-	initData()
+	//initData()
+	//initJsonData()
+	initJsonData2()
+	//initCsvData()
+	//initGobData()
+}
+
+func initJsonData2() {
+	// 初始化
+	var books map[int]*Book = make(map[int]*Book)
+	book1 := &Book{Id: 1, Title: "Title", Summary: "Summary", Author: "ycp"}
+	books[book1.Id] = book1
+
+	//json 序列化
+	data, _ := json.Marshal(books)
+
+	file1, _ := os.Create("books.json")
+	defer file1.Close()
+	_, err := file1.Write(data)
+
+	if err != nil {
+		panic(err)
+	}
+
+	file2, _ := os.Open("books.json")
+	defer file2.Close()
+	dataEncode := make([]byte, len(data))
+	file2.Read(dataEncode)
+
+	var booksEncode map[int]*Book
+	json.Unmarshal(dataEncode, &booksEncode)
+
+	fmt.Printf("%#v", booksEncode[book1.Id])
+	//fmt.Println(booksEncode)
+}
+
+func initCsvData() {
+
+}
+func initGobData() {
+
+}
+
+type Book struct {
+	Id      int    `json:"id"`
+	Title   string `json:"title"`
+	Summary string `json:"summary"`
+	Author  string `json:"author"`
+}
+
+func initJsonData() {
+	// 初始化
+	var books map[int]*Book = make(map[int]*Book)
+	book1 := &Book{Id: 1, Title: "Title", Summary: "Summary", Author: "ycp"}
+	books[book1.Id] = book1
+
+	//json 序列化
+	data, _ := json.Marshal(books)
+	err := ioutil.WriteFile("books.json", data, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
+	dataEncode, _ := ioutil.ReadFile("books.json")
+	var booksEncode map[int]*Book
+	json.Unmarshal(dataEncode, &booksEncode)
+
+	fmt.Println(booksEncode)
 }
 
 type Post struct {
