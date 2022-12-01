df431519bbab3992128670b49d354ac642ccf92d
ycp <820363773@qq.com>
2022-12-01 21:39:04 +0800

    update beego demo

--- project/beego/demo_api/controllers/login.go
+++ project/beego/demo_api/controllers/login.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"demo_api/models"
 	"encoding/json"
+	"github.com/beego/beego/v2/core/logs"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -36,4 +37,30 @@ func (ctrl *LoginController) Login() {
 		ctrl.Data["json"] = ReturnError(7001, "登录失败！")
 	}
 	ctrl.ServeJSON()
+}
+
+func (this *LoginController) GetUserList() {
+
+	req := make(map[string]int)
+	data := this.Ctx.Input.RequestBody //在RequestBody中读取Json
+	json.Unmarshal(data, &req)
+
+	logs.Info("%v", req)
+	//limit, _ := strconv.Atoi(req["limit"])
+	//offset, _ := strconv.Atoi(req["offset"])
+	//remember_token, _ := strconv.Atoi(req["remember_token"])
+	limit, _ := req["limit"]
+	offset, _ := req["offset"]
+	remember_token, _ := req["remember_token"]
+	if limit == 0 {
+		limit = 2
+	}
+
+	count, users, err := models.GetUserList(limit, offset, remember_token)
+	if err == nil {
+		this.Data["json"] = ReturnSuccess(0, "获取成功！", users, count)
+	} else {
+		this.Data["json"] = ReturnError(7001, "获取失败！")
+	}
+	this.ServeJSON()
 }
--- project/beego/demo_api/models/login.go
+++ project/beego/demo_api/models/login.go
@@ -40,3 +40,19 @@ func FindUser(email string, password string) (int64, string) {
 	}
 	return user.Id, user.Name
 }
+func GetUserList(limit int, offset int, remember_token int) (int64, []orm.Params, error) {
+	var o orm.Ormer
+	o = orm.NewOrm()
+
+	var users []orm.Params
+
+	query := o.QueryTable("users")
+	query = query.Filter("remember_token", remember_token)
+	query = query.OrderBy("-created_at")
+
+	count, _ := query.Values(&users)
+
+	query = query.Limit(limit, offset)
+	_, err := query.Values(&users)
+	return count, users, err
+}
