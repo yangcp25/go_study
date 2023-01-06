a5964784342cdd2c5c27ff93ce2db65eaeac368d
ycp <820363773@qq.com>
2023-01-06 18:05:46 +0800

    update

---
+++ design_pattern/design_pattern_do/factory/factory.go
@@ -0,0 +1,28 @@
+package factory
+
+type IRuleConfigParser interface {
+	Parse(data []byte)
+}
+
+type JsonRuleConfigParser struct{}
+
+func (jsonParser JsonRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+type YamlRuleConfigParser struct {
+}
+
+func (yamlParser YamlRuleConfigParser) Parse(data []byte) {
+	panic("implement me2")
+}
+
+func NewIRuleConfigParser(t string) IRuleConfigParser {
+	switch t {
+	case "json":
+		return JsonRuleConfigParser{}
+	case "yaml":
+		return YamlRuleConfigParser{}
+	}
+	return nil
+}
