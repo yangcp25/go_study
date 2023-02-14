37ea75c3522e3356cb4458025d3fe5c8f3ce25fc
ycp <820363773@qq.com>
2023-02-14 17:59:55 +0800

    update

--- advanced/fromwork/main.go
+++ advanced/fromwork/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"fromwork/web"
 	"net/http"
 )
 
@@ -9,5 +12,84 @@ func main() {
 		str := []byte{'h', 'e', 'l', 'l', '0'}
 		writer.Write(str)
 	})
+
+	http.HandleFunc("/test2", handler2)
+	http.HandleFunc("/test3", handler3)
+	http.HandleFunc("/returnJson", handler4)
+	http.HandleFunc("/returnJson2", handler5)
 	http.ListenAndServe("localhost:8081", nil)
+}
+
+func handler5(writer http.ResponseWriter, request *http.Request) {
+	context := &web.Context{
+		request, writer,
+	}
+
+	var data map[string]interface{}
+
+	context.ReadJson(&data)
+
+	context.WriteJson(1, "成功", data)
+}
+
+func handler4(writer http.ResponseWriter, request *http.Request) {
+	context := &web.Context{
+		request, writer,
+	}
+
+	var data map[string]interface{}
+
+	context.ReadJson(&data)
+
+	//
+	jsonData := map[string]interface{}{
+		"code": 4,
+		"msg":  "xxxxx",
+		"data": data,
+	}
+
+	res, _ := json.Marshal(jsonData)
+
+	_, err := writer.Write(res)
+	if err != nil {
+		fmt.Errorf("err" + err.Error())
+	}
+}
+
+// 获取参数
+func handler2(writer http.ResponseWriter, request *http.Request) {
+	// body 获取
+	/*body := request.Body
+	data, _ := ioutil.ReadAll(body)
+
+	var params map[string]interface{}
+	err := json.Unmarshal(data, &params)
+	if err != nil {
+		fmt.Println("err:" + err.Error())
+		return
+	}
+	fmt.Println(params)*/
+	// form 获取 文件等
+	//request.ParseMultipartForm(19999)
+	//
+	//fmt.Println(request.MultipartForm)
+	//request.ParseForm()
+
+	//fmt.Println(request.Form)
+	//
+	//// 获取query
+	//data := request.URL.Query()
+	//
+	//fmt.Println(data["test"])
+}
+
+// 封装数据获取
+
+func handler3(writer http.ResponseWriter, request *http.Request) {
+	context := &web.Context{request, writer}
+
+	var data map[string]interface{}
+	context.ReadJson(&data)
+
+	fmt.Println(data)
 }
---
+++ advanced/fromwork/web/context.go
@@ -0,0 +1,59 @@
+package web
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Context struct {
+	R *http.Request
+	W http.ResponseWriter
+}
+
+func (c *Context) ReadJson(data interface{}) {
+	body := c.R.Body
+
+	res, err := ioutil.ReadAll(body)
+
+	if err != nil {
+		_ = fmt.Errorf("读取数据错误！")
+	}
+
+	err = json.Unmarshal(res, data)
+	if err != nil {
+		_ = fmt.Errorf("序列化失败！")
+	}
+}
+
+func (c Context) WriteJson(status int8, msg string, data interface{}) error {
+	//
+	//jsonData := msgInfo{
+	//	code: status,
+	//	msg:  msg,
+	//	data: data,
+	//}
+	jsonData := map[string]interface{}{
+		"code": status,
+		"msg":  msg,
+		"data": data,
+	}
+	res, err := json.Marshal(jsonData)
+
+	if err != nil {
+		return fmt.Errorf("序列化失败")
+	}
+	_, err = c.W.Write(res)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type msgInfo struct {
+	code int8
+	msg  string
+	data interface{}
+}
