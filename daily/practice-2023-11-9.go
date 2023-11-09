666da41c5651b62606a4ed2531232a573182796a
ycp <820363773@qq.com>
2023-11-09 22:19:32 +0800

    [feat] daily

---
+++ daily/practice-2023-11-9.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	//https://chai2010.cn/advanced-go-programming-book/index.html
+	//for range 对字符串的迭代模拟实现
+}
--- review/switch/switch.go
+++ review/switch/switch.go
@@ -6,7 +6,7 @@ func main() {
 	a := 3
 
 	switch a {
-	case a=3:
+	case a:
 		fmt.Print("1")
 	case 3:
 		fmt.Print("1")
