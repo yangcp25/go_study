b24a944029ac92e8eb05143e22450a01ce46ad90
ycp <820363773@qq.com>
2023-12-04 21:59:28 +0800

    [feat] add queue array and slice version

--- data_structure_algorithm_beautiful/queue/array/queue.go
+++ data_structure_algorithm_beautiful/queue/array/queue.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	stack1 := createStack[int](3)
+	// 创建一个简单队列
+	// 如果head == tail 队列空
+	// 如果tail == len(array) - 1
+	// 整体做迁移 如果head == 0 队列满
+	stack1 := createQueue[int]()
 	err := stack1.push(1)
 	// 处理错误 后面的就不处理了
 	if err != nil {
@@ -18,49 +22,70 @@ func main() {
 	stack1.push(5)
 
 	popData1 := stack1.pop()
-	fmt.Printf("弹出栈元素%+v\n", *popData1)
+	fmt.Printf("出队列元素%+v\n", *popData1)
 	popData2 := stack1.pop()
-	fmt.Printf("弹出栈元素%+v\n", *popData2)
+	fmt.Printf("出队列元素%+v\n", *popData2)
 
 	stack1.push(6)
 	stack1.push(7)
-
+	stack1.push(8)
+	stack1.push(9)
+	stack1.pop()
+	err = stack1.push(10)
+	// 处理错误 后面的就不处理了
+	if err != nil {
+		fmt.Printf(err.Error() + "\n")
+	}
 	stack1.forEach()
 }
 
-type stack[T int | string | map[string]string] struct {
-	data  []T
-	limit int
+// 默认小一点的空间 size 为5 数组空间=size+1
+type queue[T int | string | map[string]string] struct {
+	data [6]T
+	head int
+	tail int
 }
 
-func createStack[T int | string | map[string]string](len int) *stack[T] {
-	return &stack[T]{
-		data:  make([]T, 0, len),
-		limit: len,
+func createQueue[T int | string | map[string]string]() *queue[T] {
+	return &queue[T]{
+		data: [6]T{},
 	}
 }
 
-func (s *stack[T]) push(item T) error {
-	if len(s.data) >= s.limit {
-		return errors.New("超过栈长度！")
+func (s *queue[T]) push(item T) error {
+	// len(s.data) - 1 这里固定为5
+	if len(s.data)-1 == s.tail {
+		if s.head == 0 {
+			return errors.New("队列满！")
+		}
+		// 做迁移
+		currentTail := s.tail - s.head
+		// 队列整体往前移动
+		for i := 0; i < currentTail; i++ {
+			s.data[i] = s.data[i+s.head]
+		}
+		s.head = 0
+		s.tail = currentTail
 	}
-	s.data = append(s.data, item)
+	s.data[s.tail] = item
+	s.tail++
 	return nil
 }
 
-func (s *stack[T]) pop() *T {
-	length := len(s.data)
-	if length == 0 {
+// 数组头部出队列
+func (s *queue[T]) pop() *T {
+	// 队列为空
+	if s.head == s.tail {
 		return nil
 	}
-	res := &s.data[length-1]
-	s.data = s.data[0 : length-1]
+	res := &s.data[s.head]
+	s.head++
 	return res
 }
 
-func (s *stack[T]) forEach() {
-	fmt.Printf("遍历栈：\n")
-	for _, item := range s.data {
-		fmt.Printf("当前栈元素%+v\n", item)
+func (s *queue[T]) forEach() {
+	fmt.Printf("遍历队列：\n")
+	for i := s.head; i < s.tail; i++ {
+		fmt.Printf("当前队列元素%+v\n", s.data[i])
 	}
 }
--- data_structure_algorithm_beautiful/queue/slice/slice.go
+++ data_structure_algorithm_beautiful/queue/slice/slice.go
@@ -1,66 +1,77 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
-	stack1 := createStack[int](3)
+	// 创建一个简单队列
+	// 如果head == tail 队列空
+	// 如果tail == len(array) - 1
+	// 整体做迁移 如果head == 0 队列满
+	stack1 := createQueue[int](5)
 	err := stack1.push(1)
 	// 处理错误 后面的就不处理了
 	if err != nil {
 		return
 	}
 	stack1.push(2)
+	fmt.Printf("队列容量%+v\n", cap(stack1.data))
 	stack1.push(3)
 	stack1.push(4)
 	stack1.push(5)
 
 	popData1 := stack1.pop()
-	fmt.Printf("弹出栈元素%+v\n", *popData1)
+	fmt.Printf("出队列元素%+v\n", *popData1)
 	popData2 := stack1.pop()
-	fmt.Printf("弹出栈元素%+v\n", *popData2)
-
+	fmt.Printf("出队列元素%+v\n", *popData2)
+	stack1.forEach()
 	stack1.push(6)
 	stack1.push(7)
-
+	stack1.push(8)
+	stack1.push(9)
+	// 看是否自动扩容
+	fmt.Printf("队列容量%+v\n", cap(stack1.data))
+	popData3 := stack1.pop()
+	fmt.Printf("出队列元素%+v\n", *popData3)
+	err = stack1.push(10)
+	// 处理错误 后面的就不处理了
+	if err != nil {
+		fmt.Printf(err.Error() + "\n")
+	}
 	stack1.forEach()
 }
 
-type stack[T int | string | map[string]string] struct {
-	data  []T
-	limit int
+type queue[T int | string | map[string]string] struct {
+	data []T
 }
 
-func createStack[T int | string | map[string]string](len int) *stack[T] {
-	return &stack[T]{
-		data:  make([]T, 0, len),
-		limit: len,
+func createQueue[T int | string | map[string]string](len int) *queue[T] {
+	return &queue[T]{
+		data: make([]T, 0, len),
 	}
 }
 
-func (s *stack[T]) push(item T) error {
-	if len(s.data) >= s.limit {
-		return errors.New("超过栈长度！")
-	}
+func (s *queue[T]) push(item T) error {
 	s.data = append(s.data, item)
 	return nil
 }
 
-func (s *stack[T]) pop() *T {
-	length := len(s.data)
-	if length == 0 {
+// 数组头部出队列
+func (s *queue[T]) pop() *T {
+	// 队列为空
+	if len(s.data) == 0 {
 		return nil
 	}
-	res := &s.data[length-1]
-	s.data = s.data[0 : length-1]
+	res := &s.data[0]
+	s.data = s.data[1:]
 	return res
 }
 
-func (s *stack[T]) forEach() {
-	fmt.Printf("遍历栈：\n")
-	for _, item := range s.data {
-		fmt.Printf("当前栈元素%+v\n", item)
+func (s *queue[T]) forEach() {
+	fmt.Printf("遍历队列：\n")
+	for _, datum := range s.data {
+		fmt.Printf("当前队列元素%+v\n", datum)
 	}
+
 }
