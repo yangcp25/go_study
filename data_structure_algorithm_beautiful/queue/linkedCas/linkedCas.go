70979402bec37b4e97ec84106e5133d371cf4d0e
ycp <820363773@qq.com>
2023-12-08 22:06:37 +0800

    [feat] update cas queue

--- data_structure_algorithm_beautiful/queue/linkedCas/linkedCas.go
+++ data_structure_algorithm_beautiful/queue/linkedCas/linkedCas.go
@@ -1,161 +1,91 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"sync"
+	"sync/atomic"
+	"unsafe"
 )
 
 // cas版本
 func main() {
-	/**
-	 (1)用原子操作去交换
-		a.拿到头结点
-		b.头结点
-	*/
+	casQueue1 := createCasQueue[int]()
+
+	casQueue1.tailPush(1)
+	casQueue1.tailPush(2)
+	casQueue1.tailPush(3)
+	casQueue1.tailPush(4)
+	casQueue1.tailPush(5)
+
+	data1 := casQueue1.headPop()
+	fmt.Printf("弹出节点：%+v\n", *data1)
+
+	casQueue1.tailPush(6)
+
+	data2 := casQueue1.headPop()
+	fmt.Printf("弹出节点：%+v\n", *data2)
+
+	casQueue1.headForeach()
 }
 
-type linked[T int | string | map[string]string] struct {
-	head     *node[T]
-	length   int
-	limit    int
-	headLock sync.Mutex
-	tailLock sync.Mutex
+type casQueue[T int | string] struct {
+	head *node[T] // 将head 申明成unsafe.Pointer 是因为后面的atomic.CompareAndSwapPointer 需要这种类型的指针
+	tail *node[T]
 }
 
-type node[T int | string | map[string]string] struct {
-	data *T
+type node[T int | string] struct {
 	next *node[T]
-	prev *node[T]
+	data T
 }
 
-func getLinked[T int | string | map[string]string](limit int) *linked[T] {
-	return &linked[T]{
-		head:     nil,
-		length:   0,
-		limit:    limit,
-		headLock: sync.Mutex{},
-		tailLock: sync.Mutex{},
+// 初始化队列
+func createCasQueue[T int | string]() *casQueue[T] {
+	return &casQueue[T]{
+		head: nil,
+		tail: nil,
 	}
 }
 
-func createNode[T int | string | map[string]string](data T) *node[T] {
+// 创建节点
+func createNode[T int | string](data T) *node[T] {
 	return &node[T]{
-		data: &data,
-		next: nil,
-		prev: nil,
+		data: data,
 	}
 }
 
-// 从头部插入
-func (l *linked[T]) headPush(data T) error {
-	l.headLock.Lock()
-	defer l.headLock.Unlock()
-	if l.length >= l.limit {
-		return errors.New("当前队满\n")
-	}
+// 尾部插入
+func (queue *casQueue[T]) tailPush(data T) bool {
 	newNode := createNode(data)
+	if queue.tail == nil {
+		atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(queue.head)), unsafe.Pointer(queue.head), unsafe.Pointer(newNode))
+		atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(queue.tail)), unsafe.Pointer(queue.tail), unsafe.Pointer(newNode))
+		return true
+	}
+	queue.tail.next = newNode
+	queue.tail = newNode
+	return true
+}
 
-	if l.head == nil {
-		l.head = newNode
-		l.length++
-		newNode.next = newNode
-		newNode.prev = newNode
+// 头部弹出
+func (queue *casQueue[T]) headPop() (node *T) {
+	if queue.head == nil {
 		return nil
 	}
-
-	currentNode := l.head
-	// 头结点位置
-	headNodePos := l.head
-
-	l.head = newNode
-	newNode.next = currentNode
-	currentNode.prev = newNode
-
-	// 找尾结点
-	for {
-		if currentNode.next == headNodePos {
-			break
-		}
-		currentNode = currentNode.next
+	node = &queue.head.data
+	queue.head = queue.head.next
+	if queue.head == nil {
+		queue.tail = nil
 	}
-
-	if l.length >= l.limit {
-		currentNode.prev.next = newNode
-		newNode.prev = currentNode.prev
-	} else {
-		currentNode.next = newNode
-		newNode.prev = currentNode
-		l.length++
-	}
-	return nil
+	return
 }
 
-// 尾部弹出
-func (l *linked[T]) tailPop() *T {
-	l.tailLock.Lock()
-	defer l.tailLock.Unlock()
-	if l.head == nil {
-		return nil
-	}
-
-	currentNode := l.head
-	// 头结点位置
-	headNodePos := l.head
-	// 找尾结点
+func (queue *casQueue[T]) headForeach() {
+	cur := queue.head
+	fmt.Print("从头部遍历当前队列：\n")
 	for {
-		if currentNode.next == headNodePos {
+		fmt.Printf("当前节点：%+v\n", cur.data)
+		if cur == queue.tail {
 			break
 		}
-		currentNode = currentNode.next
-	}
-
-	//currentNode.prev.next = headNodePos
-	//headNodePos.prev = currentNode.prev
-	if currentNode == headNodePos {
-		// The list has only one element
-		l.head = nil
-	} else {
-		currentNode.prev.next = headNodePos
-		headNodePos.prev = currentNode.prev
-	}
-
-	l.length--
-	return currentNode.data
-}
-
-// 从头部遍历
-func (l *linked[T]) headForeach() {
-	if l.head == nil {
-		fmt.Printf("队空：\n")
-		return
-	}
-	headNode := l.head
-	headNodPos := headNode
-	fmt.Printf("从头结点遍历：\n")
-	for {
-		fmt.Printf("当前节点: %+v\n", *headNode.data)
-		if headNode.next == headNodPos {
-			break
-		}
-		headNode = headNode.next
-	}
-}
-
-// 从尾部遍历
-func (l *linked[T]) tailForeach() {
-	if l.head == nil {
-		fmt.Printf("队空：\n")
-		return
-	}
-	endNode := l.head
-	endNodePos := endNode
-	fmt.Printf("从尾结点遍历：\n")
-	for {
-		fmt.Printf("当前节点: %+v\n", *endNode.prev.data)
-		if endNode.prev == endNodePos {
-			break
-		}
-		endNode = endNode.prev
+		cur = cur.next
 	}
 }
