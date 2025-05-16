d8d351b643251d4cc3302902d4f1ac64f7a90db1
ycp <820363773@qq.com>
2025-05-16 09:14:18 +0800

    [feat] update regexp

--- review/regexp/test2.go
+++ review/regexp/test2.go
@@ -11,8 +11,14 @@ func main() {
 
 	fmt.Println(res)
 
-	rege2 := regexp.MustCompile(`(^[0-9]|^1[0-9][0-9]?|^2[0-4][0-9]|^25[0-5])\.([0-9]|1[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.([0-9]|1[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.([0-9]|1[0-9][0-9]?|2[0-4][0-9]|25[0-5])`)
-	res2 := rege2.FindAllStringSubmatch("1.1.1.1", -1)
+	rege2 := regexp.MustCompile(`(^[0-9]|^1[0-9]?[0-9]?|^2[0-4][0-9]|^25[0-5])`)
+	res2 := rege2.FindAllStringSubmatch("100", -1)
 
 	fmt.Println(res2)
+
+	rege3 := regexp.MustCompile(`^1+0+$`)
+
+	res3 := rege3.FindAllStringSubmatch("111100", -1)
+
+	fmt.Println(res3)
 }
