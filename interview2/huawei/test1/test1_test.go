78362b643d5b5eeb031e5a6f1bd3b6e08381a798
ycp <820363773@qq.com>
2025-08-20 09:58:35 +0800

    [feat] update daily

--- interview2/huawei/test1/test1_test.go
+++ interview2/huawei/test1/test1_test.go
@@ -3,23 +3,51 @@ package test1
 import "testing"
 
 func TestSubStrLasPos(t *testing.T) {
-	type args struct {
-		str string
-		sub string
-	}
 	tests := []struct {
 		name string
-		args args
+		L    string
+		S    string
 		want int
 	}{
-		// 基础功能测试
-		{"Single char match", args{"abcdc", "abc"}, 4},
+		{"S is empty", "abc", "", -1},
+		{"L is empty", "", "abc", -1},
+		{"S equals L", "abc", "abc", 2},
+		{"S longer than L", "abc", "abcd", -1},
+		{"Single char match", "a", "a", 0},
+		{"Single char no match", "a", "b", -1},
+		{"Multiple same chars", "aa", "aa", 1},
+		{"Multiple chars missing one", "ab", "aa", -1},
+		{"Valid subsequence", "abcde", "ace", 4},
+		{"Invalid subsequence", "abcde", "aec", -1},
+		{"Another valid case", "abcdc", "abc", 4},
+		{"Another invalid case", "aa", "ab", -1},
+		{"Large L no match", "abcdefghijklmnopqrstuvwxyz", "zz", -1},
+		{"Large L valid", "abcdefghijklmnopqrstuvwxyz", "az", 25},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := SubStrLasPos(tt.args.str, tt.args.sub); got != tt.want {
-				t.Errorf("SubStrLasPos() = %v, want %v", got, tt.want)
+			if got := SubStrLasPos(tt.L, tt.S); got != tt.want {
+				t.Errorf("SubStrLasPos(%q, %q) = %v, want %v", tt.L, tt.S, got, tt.want)
 			}
 		})
+	}
+}
+
+func BenchmarkSubStrLasPos(b *testing.B) {
+	// 创建大规模测试数据
+	L := make([]byte, 500000)
+	for i := range L {
+		L[i] = 'a'
+	}
+	// 在末尾添加一些不同的字符
+	L[499999] = 'b'
+	L[499998] = 'c'
+
+	S := "abc"
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		SubStrLasPos(string(L), S)
 	}
 }
