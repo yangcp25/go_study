66a1f8fca05575926c2ac534a3e86649a6476517
ycp <820363773@qq.com>
2025-09-20 10:12:13 +0800

    [feat] update daily

--- algorithm/heap/ipo/heap1.go
+++ algorithm/heap/ipo/heap1.go
@@ -3,8 +3,47 @@ package main
 import (
 	heap2 "container/heap"
 	"fmt"
+	"sort"
 )
 
+/*
+*
+
+代码
+测试用例
+测试结果
+测试结果
+502. IPO
+已解答
+困难
+相关标签
+premium lock icon
+相关企业
+假设 力扣（LeetCode）即将开始 IPO 。为了以更高的价格将股票卖给风险投资公司，力扣 希望在 IPO 之前开展一些项目以增加其资本。 由于资源有限，它只能在 IPO 之前完成最多 k 个不同的项目。帮助 力扣 设计完成最多 k 个不同项目后得到最大总资本的方式。
+
+给你 n 个项目。对于每个项目 i ，它都有一个纯利润 profits[i] ，和启动该项目需要的最小资本 capital[i] 。
+
+最初，你的资本为 w 。当你完成一个项目时，你将获得纯利润，且利润将被添加到你的总资本中。
+
+总而言之，从给定项目中选择 最多 k 个不同项目的列表，以 最大化最终资本 ，并输出最终可获得的最多资本。
+
+答案保证在 32 位有符号整数范围内。
+
+示例 1：
+
+输入：k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+输出：4
+解释：
+由于你的初始资本为 0，你仅可以从 0 号项目开始。
+在完成后，你将获得 1 的利润，你的总资本将变为 1。
+此时你可以选择开始 1 号或 2 号项目。
+由于你最多可以选择两个项目，所以你需要完成 2 号项目以获得最大的资本。
+因此，输出最后最大化的资本，为 0 + 1 + 3 = 4。
+示例 2：
+
+输入：k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
+输出：6
+*/
 func main() {
 	res := findMaximizedCapital(2, 0, []int{1, 2, 3}, []int{0, 1, 1})
 	fmt.Println(res)
@@ -23,11 +62,13 @@ func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
 	projects := make([]project, 0)
 	for key, val := range capital {
 		projects = append(projects, project{
-			key:         key,
-			profit:      val,
-			pureProfits: profits[key],
+			capital: val,
+			profit:  profits[key],
 		})
 	}
+	sort.Slice(projects, func(i, j int) bool {
+		return projects[i].capital < projects[j].capital
+	})
 
 	//
 	check := make(map[int]bool)
@@ -38,15 +79,15 @@ func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
 		for i := 0; i < k; i++ {
 			if capital[i] <= w && !check[i] {
 				heap2.Push(heapMax, project{
-					key:         i,
-					pureProfits: profits[i] - capital[i],
-					profit:      profits[i],
+					key:     i,
+					capital: profits[i] - capital[i],
+					profit:  profits[i],
 				})
 			}
 		}
 		if heapMax.Len() > 0 {
 			project := heap2.Pop(heapMax).(project)
-			w += project.pureProfits
+			w += project.capital
 			check[project.key] = true
 		} else {
 			break
@@ -63,9 +104,9 @@ func Max(a, b int) int {
 }
 
 type project struct {
-	key         int
-	pureProfits int
-	profit      int
+	key     int
+	capital int
+	profit  int
 }
 type MaxHeap []project
 
@@ -74,7 +115,7 @@ func (m MaxHeap) Len() int {
 }
 
 func (m MaxHeap) Less(i, j int) bool {
-	return m[i].pureProfits > m[j].pureProfits
+	return m[i].capital > m[j].capital
 }
 
 func (m MaxHeap) Swap(i, j int) {
