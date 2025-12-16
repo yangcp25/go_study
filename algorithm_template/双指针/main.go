26dec40c6ee0fb8cabe56df2f8dc708e1a06bca6
ycp <820363773@qq.com>
2025-12-16 09:02:18 +0800

    [feat] update daily

--- algorithm_template/双指针/main.go
+++ algorithm_template/双指针/main.go
@@ -88,6 +88,9 @@ func Min(a, b int) int {
 }
 
 // 42 接雨水
+/**
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+*/
 func trap(height []int) int {
 	left, right := 0, len(height)-1
 	leftMax, rightMax := 0, 0
