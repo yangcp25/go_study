c24c28d2cf17fad326c66282633e5281ffa8eebc
ycp <820363773@qq.com>
2026-01-30 09:01:08 +0800

    [feat] update daily

--- algorithm_template/集合1-200/main.go
+++ algorithm_template/集合1-200/main.go
@@ -576,3 +576,37 @@ func removeElement(nums []int, val int) int {
 	}
 	return i
 }
+
+// 41 缺失的第一个正数
+
+func firstMissingPositive(nums []int) int {
+	n := len(nums)
+	// 找到每个数的位置
+	for i := 0; i < n; i++ {
+		for nums[i] >= 1 && nums[i] <= n && nums[nums[i]-1] != nums[i] {
+			nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+		}
+	}
+	//
+	for i := 0; i < n; i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+	return n + 1
+}
+
+// 55 跳跃游戏
+func canJump(nums []int) bool {
+	maxReach := 0
+
+	for i := 0; i < len(nums); i++ {
+		if i > maxReach {
+			return false
+		}
+		if maxReach < i+nums[i] {
+			maxReach = i + nums[i]
+		}
+	}
+	return true
+}
